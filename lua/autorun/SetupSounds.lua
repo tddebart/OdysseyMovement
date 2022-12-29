7bee370b988f4d8e4832e0f2c669faa2c4c8bb3e
tddebart <tddebart@gmail.com>
2022-12-29 23:25:21 +0100

    added sounds !! 🥳 and small ground pound bugfixes

---
+++ lua/autorun/SetupSounds.lua
@@ -0,0 +1,87 @@
+sound.Add({
+    name = "player_jump1",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = {"player/Jump00.mp3", "player/Jump01.mp3", "player/Jump02.mp3", "player/Jump04.mp3", "player/Jump06.mp3", "player/Jump08.mp3"}
+})
+
+sound.Add({
+    name = "player_jump2",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = {"player/Jump10.mp3", "player/Jump11.mp3"}
+})
+
+sound.Add({
+    name = "player_jump3",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = {"player/JumpHigh00.mp3", "player/JumpHigh01.mp3", "player/JumpHigh02.mp3"}
+})
+
+sound.Add({
+    name = "player_longjump",
+    channel = CHAN_STATIS,
+    volume = 1.0,
+    level = 80,
+    sound = "player/JumpBroad.mp3"
+})
+
+sound.Add({
+    name = "player_dive",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = {"player/Dive00.mp3","player/Dive01.mp3","player/Dive02.mp3","player/Dive03.mp3"}
+})
+
+sound.Add({
+    name = "player_bonk",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = "player/Bonk.mp3"
+})
+
+sound.Add({
+    name = "player_pound_pre",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 60,
+    sound = "player/PoundPre.mp3"
+})
+
+sound.Add({
+    name = "player_pound_land",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = "player/PoundLand.mp3"
+})
+
+sound.Add({
+    name = "player_pound_jump",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = {"player/PoundJump00.mp3","player/PoundJump01.mp3","player/PoundJump02.mp3"}
+})
+
+sound.Add({
+    name = "player_wallslide",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = "player/WallSlide.mp3"
+})
+
+sound.Add({
+    name = "player_walljump",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    sound = "player/WallJump.mp3"
+})
--- lua/autorun/g_movement.lua
+++ lua/autorun/g_movement.lua
@@ -2,12 +2,24 @@
 CreateConVar('odysseyMovement_roll_maxVelocity', 450, 'The maximum velocity you can get from rolling')
 
 CreateConVar('odysseyMovement_walljump_launchVelocityAway', -300, 'The horizontal velocity you get when walljumping away from the wall')
-CreateConVar('odysseyMovement_walljump_launchVelocityUp', 250, 'The upwards velocity you get when walljumping away from the wall')
+CreateConVar('odysseyMovement_walljump_launchVelocityUp', 300, 'The upwards velocity you get when walljumping away from the wall')
 
 CreateConVar('odysseyMovement_backflip_height', 400.0, 'The height of the backflip')
 
 CreateConVar('odysseyMovement_longjump_height', 600.0, 'The height of the longjump')
 CreateConVar('odysseyMovement_longjump_distance', 300.0, 'The distance of the longjump')
+
+local jumpSound = Sound('player_jump1')
+local longJumpSound = Sound('player_longjump')
+local diveSound = Sound('player_dive')
+local bonkSound = Sound('player_bonk')
+local poundPreSound = Sound('player_pound_pre')
+local poundLandSound = Sound('player_pound_land')
+local poundJumpSound = Sound('player_pound_jump')
+local wallSlideSound = Sound('player_wallslide')
+local wallJumpSound = Sound('player_walljump')
+
+local loopId = 0
 
 hook.Add('Move', 'OdyMove', function(ply, mv)
 
@@ -67,6 +79,8 @@ function landed (ply, mv)
     if ply:GetIsGroundPounding() then
         ply:SetRollVelocity(GetConVar('odysseyMovement_roll_maxVelocity'):GetInt())
 
+        ply:EmitSound(poundLandSound)
+
         // Force crouch by +duck and -duck
         // TODO: need to find a way to force crouch without using concommands
         // because then you can't use the crouch key to uncrouch
@@ -85,24 +99,45 @@ function landed (ply, mv)
 
     ply:SetIsLongJumping(false)
     ply:SetIsDiving(false)
+
+    if loopId != 0 then
+        ply:StopLoopingSound(loopId)
+    end
+
+    // Reset roll velocity if we are not rolling after 0.3 seconds
+    timer.Simple(0.3, function()
+        if not ply:GetRollUpdate() then
+            ply:SetRollVelocity(0)
+        end
+    end)
 end
 
 function jumped (ply, mv)
     // Walljump
-    if ply:GetWallDirection() != Vector(0,0,0) then
+    if ply:GetWallDirection() != Vector(0,0,0) and not ply:GetIsGroundPounding() then
         local vel = ply:GetWallDirection() * GetConVar('odysseyMovement_walljump_launchVelocityAway'):GetInt()
         vel.z = GetConVar('odysseyMovement_walljump_launchVelocityUp'):GetInt()
         mv:SetVelocity(vel)
         ply:SetWallDirection(Vector(0,0,0))
 
+        ply:EmitSound(wallJumpSound)
+        ply:EmitSound(jumpSound)
+
     end
 
     if not ply:IsOnGround() then return end
 
+
     // Ground pound jump
     if ply:GetIsGroundPounding() then
+        ply:SetIsGroundPounding(false)
+        ply:SetMaxSpeedOverride(0)
         ply:ConCommand('-duck')
-        mv:SetVelocity(Vector(0,0,400))
+
+        mv:SetVelocity(Vector(0,0,450))
+
+        ply:EmitSound(poundJumpSound)
+        return
     end
 
     // Backflip or longjump
@@ -116,26 +151,42 @@ function jumped (ply, mv)
             vec.z = GetConVar('odysseyMovement_backflip_height'):GetFloat() / len
 
             mv:SetVelocity(vec * len)
+            ply:EmitSound(poundJumpSound)
+            return
         elseif ply:KeyDown(IN_DUCK) then // Longjump
             len = GetConVar('odysseyMovement_longjump_distance'):GetFloat()
             vec.z = GetConVar('odysseyMovement_longjump_height'):GetFloat() / len
             ply:SetIsLongJumping(true)
 
             ply:SetVelocity(vec * len)
+
+            ply:SetRollVelocity(GetConVar('odysseyMovement_roll_maxVelocity'):GetInt())
+
+            ply:EmitSound(longJumpSound)
+
+            return
         end
 
     end
+
+    ply:EmitSound(jumpSound)
 end
 
 function ducked (ply, mv)
     // Groundpound
-    if not ply:IsOnGround() and not ply:GetIsLongJumping() and not ply:GetIsDiving() then
+    if
+     not ply:IsOnGround()
+     and not ply:GetIsLongJumping()
+     and not ply:GetIsDiving()
+     and not ply:GetIsGroundPounding() then
         ply:SetMaxSpeedOverride(1)
         ply:SetFreezePlayer(true)
         ply:SetIsGroundPounding(true)
         mv:SetVelocity(Vector(0,0,0))
+
+        ply:EmitSound(poundPreSound)
         timer.Simple(0.3, function()
-            if ply:GetIsGroundPounding() then
+            if not ply:GetIsDiving() then
                 ply:SetFreezePlayer(false)
                 ply:SetVelocity(Vector(0,0,-500))
             end
@@ -156,8 +207,8 @@ function use (ply, mv)
             ply:SetRollUpdate(true)
             roll(ply, mv)
         end
-    elseif ply:GetIsGroundPounding() and not ply:IsOnGround() then // Dive
-        print('dive')
+    // Dive
+    elseif ply:GetIsGroundPounding() and not ply:IsOnGround() then
         ply:SetFreezePlayer(false)
         ply:SetMaxSpeedOverride(0)
         local vec = ply:GetAimVector()
@@ -169,6 +220,8 @@ function use (ply, mv)
         ply:SetIsGroundPounding(false)
         ply:ConCommand('-duck')
         ply:SetIsDiving(true)
+
+        ply:EmitSound(diveSound)
     end
 end
 
@@ -180,9 +233,10 @@ function wallJumpCheck (ply, mv)
 
     if wallDir == Vector(0,0,0) then
         wallDir = ply:GetAimVector()
-    else
+    elseif vel.z < 0 then
         vel.z = vel.z + 5
         mv:SetVelocity(vel)
+        loopId = ply:StartLoopingSound(wallSlideSound)
     end
 
     wallDir.z = 0
@@ -202,6 +256,7 @@ function wallJumpCheck (ply, mv)
         ply:SetWallDirection(wallDir)
     else
         ply:SetWallDirection(Vector(0,0,0))
+        ply:StopLoopingSound(loopId)
     end
 end
 
